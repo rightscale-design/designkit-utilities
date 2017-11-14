80146f0b437f74bd9f0417d0f29daf2a02162d18
jasonmelgoza <jason@rightscale.com>
2017-11-13 17:06:53 -0800

    DK - Added text-transform rules to address #3

---
+++ scripts/scss.js
@@ -0,0 +1,25 @@
+var sass = require('node-sass');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+// Compile Sass
+sass.render({
+  file: './src/index.scss',
+  outputStyle: 'expanded',
+  includePaths: [
+    'node_modules/bourbon/app/assets/stylesheets',
+    'node_modules/designkit-colors/src/scss',
+  ]
+}, function(error, result) {
+  if (error) {
+    console.error(error);
+    return process.exit(1);
+  } else {
+    mkdirp('./dist/temp');
+    fs.writeFile('./dist/temp/_temp.css', result.css, function(err) {
+      if (!err) {
+        return console.log('Success!');
+      }
+    })
+  }
+});
